d2d0b1cd6f0b671692ea890f36d105e04b860a99
irycz <iryczek35@gmail.com>
2021-03-29 22:40:12 +0200

    Klasa User, jest szansa że będzie trzeba do tego wrócić bo nie wiem czy nie trzeba obsługe wyjątków tutaj gdzieś dodać (ale chyba nie).

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-print("Karol to baran")
+import hashlib
 
+class User:
+    ''' Klasa z konstruktorem usera, wywouje sha256 za pomocą metody klasy
+        _encrypt_password, check_password sprawdza czy hasło podane jako argument
+        jest równy zapisanemu w obiekcie'''
 
-print("to jest test z pushem")
+    def __init__(self, name, password):
+        self.username = self._encrypt_password(name)
+        self.password = self._encrypt_password(password)
+        self.is_logged = 0
 
+    def _encrypt_password(self, my_string):
+        return hashlib.sha256(my_string)
 
-print("nie ma mowy")
-#komentarz
+    def ckeck_password(self, password):
+        if hashlib.sha256(password) == self.password:
+            return 1
+        return 0
+        #tutaj wyjątki
 
-print("add first change")
-print("add second change")
